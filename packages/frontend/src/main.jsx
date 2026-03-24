b96b4cdb904ec832552d00fcefa365a95d663b59
Jacques Ellis <ellis.jacques@gmail.com>
2026-03-24 15:03:33 +1300

    main: wire ApolloProvider and import Tailwind globals

--- packages/frontend/src/main.jsx
+++ packages/frontend/src/main.jsx
@@ -1,14 +1,11 @@
-import './styles/globals.css';
-import React from 'react';
+import '@todo-app/ui/src/styles/globals.css';
 import ReactDOM from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
-import { client } from './apollo/client';
-import App from './App';
+import { client } from './apollo/client.js';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
 );
