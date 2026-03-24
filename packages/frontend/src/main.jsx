9a5bc5a4e4b62753dce16ff4aa249a95281b2a07
Jacques Ellis <ellis.jacques@gmail.com>
2026-03-24 15:10:04 +1300

    main: wrap in StrictMode and guard against missing #root

--- packages/frontend/src/main.jsx
+++ packages/frontend/src/main.jsx
@@ -1,11 +1,22 @@
 import '@todo-app/ui/src/styles/globals.css';
-import ReactDOM from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
-import { client } from './apollo/client.js';
-import App from './App.jsx';
+import { client }         from './apollo/client';
+import App                from './App';
+import ReactDOM           from 'react-dom/client';
+import React              from 'react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+const rootEl = document.getElementById('root');
+
+if (!rootEl) {
+  throw new Error(
+    '[main] Could not find #root element. Check index.html.'
+  );
+}
+
+ReactDOM.createRoot(rootEl).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
 );
