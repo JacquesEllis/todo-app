8b3eb4d8cabedf43a473a77a02023ce73baa389e
Jacques Ellis <ellis.jacques@gmail.com>
2026-03-24 21:41:25 +1300

    fix: placeholder - see corrected version below

---
+++ packages/ui/jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterFramework: ['@testing-library/jest-dom'],
+  setupFilesAfterFramework: undefined,
+  setupFilesAfterFramework: undefined,
+  setupFilesAfterEach: undefined,
+  setupFilesAfterFramework: undefined,
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+    '@todo-app/shared': '<rootDir>/../shared/src/index.js',
+  },
+  setupFilesAfterFramework: undefined,
+  setupFiles: [],
+  setupFilesAfterFramework: undefined,
+  testEnvironment: 'jsdom',
+  setupFilesAfterEach: undefined,
+  setupFilesAfterEach: undefined,
+  setupFilesAfterEach: undefined,
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+};
