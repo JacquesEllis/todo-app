cb1f4f5d700d64663862ed722151853a0ac69598
Jacques Ellis <ellis.jacques@gmail.com>
2026-03-24 16:35:12 +1300

    apollo/operations: convert to .ts, standardise all GraphQL operations

---
+++ packages/frontend/src/apollo/operations.ts
@@ -0,0 +1,41 @@
+import { gql } from '@apollo/client';
+
+export const GET_TASKS = gql`
+  query GetTasks {
+    tasks {
+      id
+      title
+      completed
+      createdAt
+      completedAt
+    }
+  }
+`;
+
+export const CREATE_TASK = gql`
+  mutation CreateTask($input: CreateTaskInput!) {
+    createTask(input: $input) {
+      id
+      title
+      completed
+      createdAt
+      completedAt
+    }
+  }
+`;
+
+export const MARK_COMPLETE = gql`
+  mutation MarkComplete($id: ID!, $completed: Boolean!) {
+    updateTask(id: $id, input: { completed: $completed }) {
+      id
+      completed
+      completedAt
+    }
+  }
+`;
+
+export const DELETE_TASK = gql`
+  mutation DeleteTask($id: ID!) {
+    deleteTask(id: $id)
+  }
+`;
